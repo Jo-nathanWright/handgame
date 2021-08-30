67b384f536e19cf929418b4c6892c345d9bef303
Jonathan Wright <j.wright72127@gmail.com>
2021-08-30 13:57:22 -0600

    Made Files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace handgame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+// To make type | dotnet new console -o 'name of file' |
