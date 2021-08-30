638a8ddf869f81effcc9595d868a514bfe68bc51
Jonathan Wright <j.wright72127@gmail.com>
2021-08-30 14:35:01 -0600

    More Setup and Some Logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace handgame
 {
@@ -6,7 +7,32 @@ namespace handgame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool deciding = true;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();
+                Console.WriteLine("Initializing Buddy!");
+                Thread.Sleep(2000);
+                Console.WriteLine("Loading your new best friend!");
+                Thread.Sleep(2000);
+                Console.WriteLine("...");
+                Thread.Sleep(5000);
+                Console.Clear();
+            while (deciding){
+                Console.WriteLine("Hi, I'm buddy! What's Your Name?");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Well Hi " + name + "! Nice to meet you! Do you want to play a game? (y/n)");
+                char choice = Console.ReadKey().KeyChar;
+                if(choice == 'n'){
+                    deciding = false;
+                    Console.WriteLine("Leaving So Soon. :( Just come Back Quick Okay?");
+                } else if (choice == 'y'){
+                    deciding = false;
+                    Console.WriteLine("So glad You want to play with me! Lets Play my favorite game, Hand Attack!");
+                } else {
+                    Console.WriteLine("That's Not A command Silly, Try Again!");
+                }
+            }
         }
     }
 }
