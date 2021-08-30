fa25c1b5d066df677bac0364c0affe7877ebc90d
Jonathan Wright <j.wright72127@gmail.com>
2021-08-30 14:47:46 -0600

    More Setup and Logic

--- Program.cs
+++ Program.cs
@@ -17,21 +17,30 @@ namespace handgame
                 Console.WriteLine("...");
                 Thread.Sleep(5000);
                 Console.Clear();
-            while (deciding){
                 Console.WriteLine("Hi, I'm buddy! What's Your Name?");
                 Console.Write("Name: ");
                 string name = Console.ReadLine();
                 Console.WriteLine("Well Hi " + name + "! Nice to meet you! Do you want to play a game? (y/n)");
-                char choice = Console.ReadKey().KeyChar;
-                if(choice == 'n'){
-                    deciding = false;
-                    Console.WriteLine("Leaving So Soon. :( Just come Back Quick Okay?");
-                } else if (choice == 'y'){
-                    deciding = false;
-                    Console.WriteLine("So glad You want to play with me! Lets Play my favorite game, Hand Attack!");
-                } else {
-                    Console.WriteLine("That's Not A command Silly, Try Again!");
-                }
+                while (deciding){
+                    char choice = Console.ReadKey().KeyChar;
+                    if(choice == 'n' ){
+                        deciding = false;
+                        Console.WriteLine();
+                        Console.WriteLine("Leaving So Soon. :( Just come Back Quick Okay?");
+                    } else if (choice == 'y'){
+                        deciding = false;
+                        Console.WriteLine();
+                        Console.Clear();
+                        Console.WriteLine("So glad You want to play with me! Lets Play my favorite game, Hand Attack!");
+                        Thread.Sleep(2500);
+                        Console.WriteLine("It's a very fun and simple game. Just Choose an Option and I'll let you know if you won! Type help for more info!");
+                        Thread.Sleep(3250);
+                        Console.WriteLine("You ready? Let's Go!");
+
+                    } else {
+                        Console.WriteLine();
+                        Console.WriteLine("That's Not A command Silly, Try Again!");
+                    }
             }
         }
     }
